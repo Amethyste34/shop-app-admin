a4603d9e7da19ee187b7ba09e7b60fe051e98869
Amethyste34 <laurence.amethyste@gmail.com>
2025-04-07 11:51:44 +0200

    Ajout du back-end avec Next.js

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+export default async function AdminLayout({
+    children,
+  }: Readonly<{
+    children: ReactNode;
+  }>) {
+
+    //Todo: Check if user is authenticated and if user is an admin before rendering the page
+
+    return (
+      <>
+        {children}
+      </>
+    );
+}
+
+//Dans Terminal : npm run dev
+//Lecture vidéo : 3:31:12 
