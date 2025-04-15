c04d8c3584ca0f0741ac707067915c6bde370c9a
Amethyste34 <laurence.amethyste@gmail.com>
2025-04-15 12:00:16 +0200

    Redirection après logout

--- src/app/admin/page.ts
+++ src/app/admin/page.ts
@@ -1,6 +1,14 @@
+import { createClient } from "@/supabase/server";
 import { redirect } from "next/navigation";
 
-const Admin = () => {
+const Admin = async () => {
+  const supabase = createClient();
+  const { data: authData } = await supabase.auth.getUser();
+
+  if (!authData?.user) {
+    return redirect("/auth");
+  }
+
   return redirect("/admin/dashboard");
 };
 
--- src/supabase/middelware.ts
+++ src/supabase/middelware.ts
@@ -39,8 +39,8 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth') &&
+    !request.nextUrl.pathname.startsWith('/admin') &&
+    !request.nextUrl.pathname.startsWith('/dashboard') &&
     request.nextUrl.pathname !== '/' //Add this line to allow the home page to be accessible to all users
   ) {
     // no user, potentially respond by redirecting the user to the login page
